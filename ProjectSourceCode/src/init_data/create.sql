822d9bcb703da2c210499053197a958c9e2210d7
Andrew Webber <acwebber01@gmail.com>
2024-11-01 00:22:54 -0600

    added basic markers and the ability to instantiate them through sql

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -1,5 +1,14 @@
-CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
-);
+CREATE TABLE
+    IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(255) NOT NULL,
+        password VARCHAR(255) NOT NULL
+    );
+
+CREATE TABLE
+    IF NOT EXISTS markers (
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(255) NOT NULL,
+        latitude FLOAT NOT NULL,
+        longitude FLOAT NOT NULL
+    );
---
+++ ProjectSourceCode/src/init_data/insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO
+    markers (title, latitude, longitude)
+VALUES
+    (
+        'Marker 1',
+        40.00717769097323,
+        -105.26803310726763
+    ),
+    (
+        'Marker 2',
+        40.006754136846695,
+        -105.26330010849537
+    );
