ff61e05d679a6c049d6b0bf4f97c95046229c28a
Andrew Webber <acwebber01@gmail.com>
2024-11-20 13:24:38 -0700

    Remove secondary marker insert

--- ProjectSourceCode/src/init_data/insert.sql
+++ ProjectSourceCode/src/init_data/insert.sql
@@ -13,20 +13,6 @@ VALUES
     );
 
 INSERT INTO
-    markers (title, latitude, longitude)
-VALUES
-    (
-        'Marker 1',
-        40.00717769097323,
-        -105.26803310726763
-    ),
-    (
-        'Marker 2',
-        40.006754136846695,
-        -105.26330010849537
-    );
-
-INSERT INTO
     events (
         event_name,
         event_date,
